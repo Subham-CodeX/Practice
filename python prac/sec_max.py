77fa72a9bad1d1598abf583b75b0bdd0c059ebf4
Subham-CodeX <stanix535@gmail.com>
2025-07-23 00:15:40 +0530

    sec_max ele

--- python prac/sec_max.py
+++ python prac/sec_max.py
@@ -1,17 +1,21 @@
-n = int(input())
-arr= list(map(int, input().split()))
-f_max=sec_max=float('-inf')
-for ele in arr:
-    if(f_max<ele):
-        f_max=ele
+# n = int(input())
+# arr= list(map(int, input().split()))
+# f_max=sec_max=float('-inf')
+# for ele in arr:
+#     if(f_max<ele):
+#         f_max=ele
 
-for item in arr:
-    if(item >f_max):
-        sec_max=f_max
-        f_max=item
-    elif(f_max > item> sec_max):
-        sec_max=item
+# for item in arr:
+#     if(item >f_max):
+#         sec_max=f_max
+#         f_max=item
+#     elif(f_max > item> sec_max):
+#         sec_max=item
    
-print(f_max)
-print(sec_max)
-print("code ended")
+# print(f_max)
+# print(sec_max)
+# print("code ended")
+n=[12,54,57,25,12]
+s=list(set(n))
+s.sort(reverse=True)
+print(s[1])
